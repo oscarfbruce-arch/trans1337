a04f28358df89788e83f697810b883e533a93c27
NocturneSterling <oscar.f.bruce@gmail.com>
2026-04-02 15:02:46 +0000

    Added csv input, restructured files

--- java/MainClass.java
+++ java/MainClass.java
@@ -1,18 +1,24 @@
+package java;
 //import java.io.*;
 //import java.nio.*;
 import java.util.*;
 public class MainClass{
     public void main(String [] args){
+        System.out.println("True for translate to leet, False for translate from leet");
         Scanner selectionScanner = new Scanner(System.in); 
         boolean selectionInput = selectionScanner.nextBoolean();
-
+        
+        System.out.println("Input csv file, text string");
+        Scanner csvScanner = new Scanner(System.in);
+        String csvString = csvScanner.nextLine();
+        
         if (selectionInput){ //takes input so it can translate either to or from leet
             System.out.println("translating to leet");
             TranslateToLeet transLeet = new TranslateToLeet(); //takes input, calls "TranslateToLeet" class and returns outcome
             Scanner inputScan = new Scanner(System.in); 
             String inputString = inputScan.nextLine();
             System.out.println("Input String: " + inputString);
-            String output = transLeet.transToLeet(inputString, "leetSimple.csv");
+            String output = transLeet.transToLeet(inputString, csvString);
             System.out.println("output: " + output);
         }
         else{
@@ -21,7 +27,7 @@ public class MainClass{
             Scanner inputScan = new Scanner(System.in);
             String inputString = inputScan.nextLine();
             System.out.println("Input String: " + inputString);
-            String output = transLeet.leetToString(inputString, "leetSimple.csv");
+            String output = transLeet.leetToString(inputString, csvString);
             System.out.println("output: " + output);
         }
     }
